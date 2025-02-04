9cf84999a360644270733f33ef6f4e3f83a44304
paveldsh1 <pavel_dsh@mail.ru>
2025-02-04 22:26:06 +0400

    upgrade Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,23 +4,23 @@ import { NavLink } from 'react-router-dom';
 
 const Header = (props) => {
     return (
-        <header className={s.header}>
-            <div className={s['header__container']}>
-                <img className={s['header__img']} src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
-                {props.isAuth ? 
-                    <div className={`${s['header__text']} badge bg-primary text-wrap`}>
-                        Login: {props.login}
-                    </div>
-                    :
-                    <NavLink to="/login">
-                        <button 
-                            type="button" 
-                            className={`${s['header__button']} btn btn-primary`}>
-                                Login
-                        </button>
-                    </NavLink>
-                }
+        <header className={s['header']}>
+            <div className={s['header__title']}>
+                <p>Social Network</p>
             </div>
+            {props.isAuth ?
+                <div className={`${s['header__login']} badge bg-primary text-wrap`}>
+                    Login: {props.login}
+                </div>
+                :
+                <NavLink to="/login">
+                    <button
+                        type="button"
+                        className={`${s['header__button']} btn btn-primary`}>
+                        Login
+                    </button>
+                </NavLink>
+            }
         </header>
     )
 }
